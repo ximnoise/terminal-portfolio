54b9de9df37b28b322ce77e0b447fde85aecf259
ximnoise <mrogge96@protonmail.com>
2022-05-27 10:34:01 +0200

    Initial Commit 🚀

---
+++ js/commands.js
@@ -0,0 +1,136 @@
+var twitter = "https://twitter.com/ximnoise";
+var linkedin = "www.linkedin.com/in/ximnoise";
+var github = "https://github.com/ximnoise";
+var bootcamp = "https://careerfoundry.com";
+var myFLixReact = "https://github.com/ximnoise/myFlix-react-client";
+var myFlixAngular = "https://github.com/ximnoise/myFlix-angular-client";
+var myFlixBackend = "https://github.com/ximnoise/myFlix-backend";
+var comgear = "https://github.com/ximnoise/comgear";
+var meet = "https://github.com/ximnoise/meet";
+var pokedex = "https://github.com/ximnoise/pokedex";
+var email = 'mailto:me@maximilianrogge.com';
+var password = "fsociety";
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>',          
+  '   Who is Ximnoise?',
+  "<br>",
+  '<span class="command">whoami</span>',         
+  '   Who are you?',
+  "<br>",
+  '<span class="command">social</span>',        
+  '   Display social networks',
+  "<br>",
+  '<span class="command">projects</span>',       
+  '   View coding projects',
+  "<br>",
+  '<span class="command">email</span>',          
+  '   Email me for your projects',
+  "<br>",
+  '<span class="command">secret</span>',         
+  '   Find the password',
+  "<br>",
+  '<span class="command">history</span>',        
+  '   View command history',
+  "<br>",
+  '<span class="command">help</span>',           
+  '   You obviously already know what this does',
+  "<br>",
+  '<span class="command">clear</span>',          
+  '   Clear terminal',
+  "<br>",
+  '<span class="command">banner</span>',         
+  '   Display the header',
+  "<br>",
+];
+
+whois = [
+  "<br>",
+  "Hey, I'm Maximilian!👋",
+  "I'm a industrial mechanic, Full-Stack-Developer and passionate 3D maker,", 
+  "who wants to finally switch careers into cybersecurity.",
+  "After completed training as industrial mechanic,", 
+  "I worked as machine operator for one of the biggest automotive supplier.",
+  "While doing that, I started to dive into web development", 
+  'and began a Full-Stack-Developer Bootcamp at <a href="' + bootcamp + '" target="_blank">Careerfoundry.' + "</a>",
+  "After 6 months I finished it and with the knowledge of an Full-Stack-Developer", 
+  "and through private events I went deeper down the rebbit hole into cybersecurity.", 
+  "So here I'm passionate about cybersecurity and 3D printing", 
+  "with the will to finally switch careers into the amazing cybersecurity community.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/ximnoise' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/ximnoise' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/ximnoise' + "</a>",
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  '<span class="command">myFlix Frontend</span>',
+  "   Angular and React application that allows users to get information about movies, genres, and directors.", 
+  "   This application uses my existing server-side REST API and MongoDB database.",
+  '   github         <a href="' + myFlixAngular + '" target="_blank">myFlix Angular' + "</a>",
+  '   github         <a href="' + myFLixReact + '" target="_blank">myFlix React' + "</a>",
+  "<br>",
+  '<span class="command">myFlix Backend</span>',
+  "   REST API that provide users with access to information about different movies, directors, and genres.", 
+  "   Users will be able to sign up, update their personal information, and create a list of their favorite movies.",
+  '   github         <a href="' + myFlixBackend + '" target="_blank">myFlix Backend' + "</a>",
+  "<br>",
+  '<span class="command">Comgear</span>',
+  "   Mobile chat app built with React Native and Expo that stores data with Google Firebase.", 
+  "   Users can send messages and are able to share images and location.",
+  '   github         <a href="' + comgear + '" target="_blank">Comgear' + "</a>",
+  "<br>",
+  '<span class="command">Meet</span>',
+  "   Meet App is a serverless, progressive web application (PWA) with React using a test-driven development (TDD) technique.", 
+  "   The application uses the Google Calendar API to fetch upcoming events. The serverless function is hosted by the cloud provider AWS.",
+  '   github         <a href="' + meet + '" target="_blank">Meet' + "</a>",
+  "<br>",
+  '<span class="command">Pokedex</span>',
+  "   This small web app is a part of my Bootcamp at CareerFoundry.", 
+  "   It contains HTML, CSS/Bootstrap and JavaScript that loads data from a external Pokemon API and show the data end points in detail.",
+  '   github         <a href="' + pokedex + '" target="_blank">Pokedex' + "</a>",
+  "<br>",
+  '<span class="command">Your project?</span>',
+  "   I can help you realizing front-end, back-end and 3D print projects.",
+  '   email          <a href="' + email + '" target="_blank">me@maximilianrogge.com' + '</a>',
+  "<br>",
+];
+
+email = [
+  "<br>",
+  'email          <a href="' + email + '" target="_blank">me@maximilianrogge.com' + '</a>',
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+banner = [
+  '<span class="index">Maximilian Rogge (Ximnoise) Not A Corporation. All rights reserved.</span>',
+  "<br>",
+  "██╗░░██╗██╗███╗░░░███╗███╗░░██╗░█████╗░██╗░██████╗███████╗",
+  "╚██╗██╔╝██║████╗░████║████╗░██║██╔══██╗██║██╔════╝██╔════╝",
+  "░╚███╔╝░██║██╔████╔██║██╔██╗██║██║░░██║██║╚█████╗░█████╗░░",
+  "░██╔██╗░██║██║╚██╔╝██║██║╚████║██║░░██║██║░╚═══██╗██╔══╝░░",
+  "██╔╝╚██╗██║██║░╚═╝░██║██║░╚███║╚█████╔╝██║██████╔╝███████╗",
+  "╚═╝░░╚═╝╚═╝╚═╝░░░░░╚═╝╚═╝░░╚══╝░╚════╝░╚═╝╚═════╝░╚══════╝  v1.0.0",
+  "<br>",
+  '<span class="color2">Welcome to my interactive portfolio web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
